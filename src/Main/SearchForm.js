dd570746e143e0ab31261a64c5161a0c2d7761e4
Cole Gibbs <colegibbs0@gmail.com>
2022-04-05 16:49:32 -0700

    MusicRecs state change functionality at submit

--- src/Main/SearchForm.js
+++ src/Main/SearchForm.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 class SearchForm extends React.Component {
+
+
+  formChange = (e) => {
+    this.setState({
+      searchQuery: e.target.searchQuery.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleFormQuery(this.state.searchQuery);
+  }
+
   render() {
     return (
-      <Form /*onSubmit={submit function}*/>
+      <Form onSubmit={this.handleSubmit}>
         <Form.Group controlId="searchQuery">
           <Form.Label>Search a Band</Form.Label>
           <Form.Control
             type="text"
             placeholder="ex: The Rolling Stones"
+            onChange={this.formChange}
           />
           <Form.Text className="text-muted">Search a band and discover similar artists!</Form.Text>
         </Form.Group>
