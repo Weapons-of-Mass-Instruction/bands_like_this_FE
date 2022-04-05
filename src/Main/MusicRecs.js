567ecdd61fe909cf5630c60079222cc9f01b1bdf
Cole Gibbs <colegibbs0@gmail.com>
2022-04-05 16:38:52 -0700

    data sorting functionality

--- src/Main/MusicRecs.js
+++ src/Main/MusicRecs.js
@@ -6,11 +6,35 @@ import SearchForm from './SearchForm';
 // There should be 3 cards in total
 
 class MusicRecs extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data:[],
+      currentSearchQuery: '',
+      searchQueries: []
+    }
+  }
+
   render() {
+    // FOR THREE CARD RENDER - ANOTHER GET ON THE BACK END IS NEEDED FOR SEARCHQUERIES(BANDS) SAVED IN THE DB
+    // let cards = this.state.searchQueries.map(searchQuery => {
+    //   let cardData = await axios.get(ServerUrlCallUsingSearchQuery)
+    //   return(
+    //     <MusicCard
+    //       searchQuery={searchQuery}
+    //       img={cardData.data.img}
+    //       name={cardData.data.name}
+    //     />
+    //   )
+    // })
     return (
       <>
-        <SearchForm />
-        <MusicCard />
+        <SearchForm/>
+        <MusicCard 
+          currentSearchQuery={this.state.currentSearchQuery}
+          img={this.state.data.img}
+          name={this.state.data.name}
+        />
       </>
     );
   }
