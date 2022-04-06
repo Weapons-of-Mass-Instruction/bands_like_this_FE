d2cea969cca86ef69e55cc20c212d74074d8d44e
Nathan Waters <nathanwaters94@gmail.com>
2022-04-06 09:30:20 -0700

    deleted elint file, added auth0 starter code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './CSSfiles/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain={process.env.REACT_APP_AUTH_DOMAIN}
+  clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+  redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
