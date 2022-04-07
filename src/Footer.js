f353428bb8f3111651eb89fb0a59a98d7441f7cb
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-04-07 15:20:53 -0700

    css styling

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,15 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 
 class Footer extends React.Component {
+
+
   render() {
     return (
+      <div className="flex-container">
+
       <Navbar className="bg-warning bg-gradient text-dark"  expand="lg">
-<Container>
+        <p> &copy; 2022 Wepons of Mass Instruction (Code Fellows 201d83final)</p>
+<Container className="fB-Fitem-1">
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -19,13 +24,14 @@ class Footer extends React.Component {
               height="40"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
-            /> 
+              /> 
             <Nav.Link href="https://github.com/Weapons-of-Mass-Instruction">GitHub
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
 </Container>
       </Navbar>
+              </div>
     );
   }
 }
