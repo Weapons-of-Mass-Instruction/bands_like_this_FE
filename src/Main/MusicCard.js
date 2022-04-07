fa968c6b28d69e3c348d53afe4ce0e00ffcdb0da
Benjamin Carter <benj.james.carter@gmail.com>
2022-04-07 09:33:59 -0700

    cleaned up notes

--- src/Main/MusicCard.js
+++ src/Main/MusicCard.js
@@ -1,18 +1,12 @@
 /* eslint-disable no-undef */
 import React from 'react';
-//import MusicCarousel from './MusicCarousel';
 import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import MusicCarousel from './MusicCarousel.js';
 import '../CSSfiles/musicCarousel.css';
 
 
 class MusicCard extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     bandToDel: ''
-  //   }
-  // }
+
 
   deleteClick = (band) => {
     this.props.findBandToDelID(band);
@@ -23,7 +17,7 @@ class MusicCard extends React.Component {
   };
 
   render() {
-    // TODO to add functionality to delete card. Deleting card should filter through bandCard prop and delete band name from array (.pop) this delete method is located in MusicRecs.js
+
 
     let cards = this.props.bandCard.map((band, index) => {
     let bandRecs = this.props.recs.filter((singleRec) => singleRec.search === band);
--- src/Main/MusicRecs.js
+++ src/Main/MusicRecs.js
@@ -21,7 +21,6 @@ class MusicRecs extends React.Component {
   }
 
   //GET all data basic skeleton
-  //url needs to be assigned correctly
   //GETs all data from the mongodb 
   getBands = async () => {
     try {
@@ -36,15 +35,12 @@ class MusicRecs extends React.Component {
   };
 
   //DELETE
-  //url needs to be assigned correctly
-  //delete is associated with the delete button
 
   deleteBand = async (id) => {
     try {
       let url = `${SERVER}/artists/${id}`;
       await axios.delete(url);
       let updatedBands = this.state.recs.filter(band => band._id !== id);
-      // keep an eye on this 
       this.setState({
         recs: updatedBands,
       });
@@ -52,10 +48,7 @@ class MusicRecs extends React.Component {
       console.error('error');
     }
   };
-  // TODO this should filter through bandCard state, and delete the string that matches the card the user pressed the delete button on (this will delete the react card from the site)
-  
-  // TODO should then filter through recs state (array) and get the ID values for the 6 objects that have a search prop that matches that same string, Pass these 6 ID values to the server to make 6 DELETE calls. 
-  // TODO once functional pass into MusicCard to be called with handle delete function.
+
   findBandToDelID = (bandToDel) => {
     // filter through bandCard and remove bandToDel
     let localBandCard = this.state.bandCard
@@ -73,14 +66,7 @@ class MusicRecs extends React.Component {
     delIdArr.forEach(id => this.deleteBand(id))
   };
 
-
-
-
   //PUT
-  //associated with the favorite button
-  // TODO: get the string of the card that was favorite-ed, use that to get the ID values of the 6 bands on that card.
-  // TODO Use the ID values of those six bands, to swap the boolean value of favorite to true.
-  // Hint: The same logic to get the card string and the 6 ID values from the Delete function will work here.
   updateBand = async (recToUpdate) => {
     try {
       console.log('first', recToUpdate);
@@ -117,7 +103,6 @@ class MusicRecs extends React.Component {
     });
     console.log(url);
   };
-
 
   handleFormQuery = (formQuery) => {
     this.setState({
