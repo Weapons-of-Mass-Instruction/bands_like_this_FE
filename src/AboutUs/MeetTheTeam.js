ecd2517ce498802ae996d899fbed1b36c54555ba
Ella Svete <ella.svete@gmail.com>
2022-04-07 21:27:12 -0700

    footer fixed

--- src/AboutUs/MeetTheTeam.js
+++ src/AboutUs/MeetTheTeam.js
@@ -6,7 +6,7 @@ class MeetTheTeam extends React.Component {
   render() {
     return(
       <>
-      <h1 id="h1Team">Meet the team</h1>
+      <h1 id="h1Team">Meet the team!</h1>
       <TeamCarousel/>
       </>
     );
